48d8d3ec286830328f3dc2b0b7b3a2d063df346f
elyzuh <katrina.elyza@gmail.com>
2024-12-06 18:09:31 +0800

    fixed: error for THM

--- 010_FirstComeFirstServe.c
+++ 010_FirstComeFirstServe.c
@@ -8,6 +8,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TRACK_SIZE 400
+#define SEEK_RATE 5 
+
 void fcfsScheduling(int requests[], int n, int head);
 
 int main() {
@@ -17,13 +20,21 @@ int main() {
     scanf("%d", &n);
 
     int requests[n];
-    printf("Enter the requests:\n");
+    printf("Enter the requests (range 0-%d):\n", TRACK_SIZE - 1);
     for (int i = 0; i < n; i++) {
         scanf("%d", &requests[i]);
+        if (requests[i] < 0 || requests[i] >= TRACK_SIZE) {
+            printf("Invalid request: %d. Requests must be between 0 and %d.\n", requests[i], TRACK_SIZE - 1);
+            return 1;
+        }
     }
 
-    printf("Enter the initial position of the disk head: ");
+    printf("Enter the initial position of the disk head (range 0-%d): ", TRACK_SIZE - 1);
     scanf("%d", &head);
+    if (head < 0 || head >= TRACK_SIZE) {
+        printf("Invalid head position. It must be between 0 and %d.\n", TRACK_SIZE - 1);
+        return 1;
+    }
 
     fcfsScheduling(requests, n, head);
 
@@ -42,6 +53,7 @@ void fcfsScheduling(int requests[], int n, int head) {
     }
     printf("End\n");
 
-    printf("Total Seek Time: %d\n", seekTime);
-    printf("Average Seek Time: %.2f\n", (float)seekTime / n);
+    printf("\nTotal Head Movement: %d\n", seekTime);
+    printf("Total Seek Time (THM * Seek Rate): %d ms\n", seekTime * SEEK_RATE);
+    printf("Average Seek Time: %.2f ms\n", (float)(seekTime * SEEK_RATE) / n);
 }
